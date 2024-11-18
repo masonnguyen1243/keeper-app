8290711e76ee34825522cef6d2f2914d42b45cd2
masonnguyen <100501225+jasoncuong@users.noreply.github.com>
2024-11-18 10:11:37 +0700

    Heading #1

---
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+
+const Profiles = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Box>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ padding: 0 }}
+          aria-controls={open ? "basic-menu-profiles" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+        >
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg"
+            alt="Avatar"
+          />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu-profiles"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button-profiles",
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+};
+
+export default Profiles;
--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,19 +1,72 @@
-import ModeSelect from "../../components/ModeSelect";
+import ModeSelect from "~/components/ModeSelect";
 import Box from "@mui/material/Box";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import AppsIcon from "@mui/icons-material/Apps";
+import { Typography } from "@mui/material";
+import WorkSpaces from "./Menus/WorkSpaces";
+import Recent from "./Menus/Recent";
+import Starred from "./Menus/Starred";
+import Templates from "./Menus/Templates";
+import TextField from "@mui/material/TextField";
+import Badge from "@mui/material/Badge";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import Tooltip from "@mui/material/Tooltip";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import Profiles from "./Menus/Profiles";
 
 const AppBar = () => {
   return (
     <>
       <Box
+        px={2}
         sx={{
-          backgroundColor: "primary.light",
           width: "100%",
           height: (theme) => theme.trello.appBarHeight,
           display: "flex",
           alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
-        <ModeSelect />
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <AppsIcon sx={{ color: "primary.main" }} />
+          <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+            <BookmarkIcon sx={{ color: "primary.main" }} />
+            <Typography
+              variant="span"
+              sx={{
+                fontSize: "1.2rem",
+                fontWeight: "bold",
+                color: "primary.main",
+              }}
+            >
+              Keeper
+            </Typography>
+          </Box>
+          <WorkSpaces />
+          <Recent />
+          <Starred />
+          <Templates />
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <TextField
+            id="outlined-search"
+            label="Search..."
+            type="search"
+            size="small"
+          />
+          <ModeSelect />
+
+          <Tooltip title="Notifications">
+            <Badge color="secondary" variant="dot" sx={{ cursor: "pointer" }}>
+              <NotificationsNoneIcon />
+            </Badge>
+          </Tooltip>
+
+          <Tooltip title="Help">
+            <HelpOutlineIcon sx={{ cursor: "pointer" }} />
+          </Tooltip>
+          <Profiles />
+        </Box>
       </Box>
     </>
   );
