49879419c00ba2e3ed700ff805299f20e936cc30
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-13 15:48:42 +0700

    Update BUILD_MODE dev and production

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,1 +1,15 @@
-export const API_ROOT = "http://localhost:8080";
+let apiRoot = "";
+
+console.log("import.meta.env", import.meta.env);
+console.log("process.env", process.env);
+
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:8080";
+}
+
+if (process.env.BUILD_MODE === "production") {
+  apiRoot = "https://keeper-api-neqe.onrender.com";
+}
+
+// export const API_ROOT = "";
+export const API_ROOT = apiRoot;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  //Cho phép Vite sử dụng process.env
+  define: {
+    "process.env": process.env,
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: "~", replacement: "/src" }],
